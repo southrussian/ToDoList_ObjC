1e1e54a0fd6e05fbb900719cb4efccbc9d5d7a21
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-02-11 18:34:49 +0300

    Created NavController with TableView and DetailViewController for creating the new event

---
+++ ToDoObjC/DetailViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  ToDoObjC
+//
+//  Created by Danil Peregorodiev on 11.02.2023.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+- (IBAction)textField:(id)sender;
+//@property (weak, nonatomic) IBOutlet UITextField *textfield;
+- (IBAction)datePicker:(id)sender;
+//@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+- (IBAction)textField:(id)sender {
+}
+- (IBAction)datePicker:(id)sender {
+}
+@end
